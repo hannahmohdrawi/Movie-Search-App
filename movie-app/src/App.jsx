e6a1325f0c2983f2a61b1e33f17e1ecb999e08ea
hannahmohdrawi <hannahmohdrawi@hotmail.com>
2025-04-02 19:17:56 +0100

    movie list added

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,16 +1,13 @@
 import './App.css'
 import MovieCard from "./components/MovieCard";
+import Home from "./pages/Home";
 
 function App() {
-  const movieNumber = 2;
+  
 
   return (
     <>
-      {movieNumber === 1 ? (
-        <MovieCard movie={{title: "Hannah's Film", release_date:"2025"}}/>
-      ) : (
-      <MovieCard movie={{title: "Mike's Film", release_date:"2025"}}/>
-      )}
+      <Home />
     </>
   )
 }
--- movie-app/src/pages/Home.jsx
+++ movie-app/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import MovieCard from "../components/MovieCard";
+import {useState} from "react";
 
 export default function Home(){
 
@@ -6,10 +8,29 @@ export default function Home(){
         {id:2, title: "Matrix", release_date: "1995"},
         {id:3, title: "Terminator", release_date: "1998"},
         
-    ]
+    ];
 
-    return 
-    <div className="home">
+    const handleSearch = () => {
 
-    </div>
+    }
+
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" 
+                placeholder = "Search for movies..." 
+                className="search-input"
+                />
+
+                <button type="submit" className="search-button">Search</button>
+            </form>
+            
+            <div className="movies-grid">
+                {movies.map((movie) => (
+                <MovieCard movie = {movie} key ={movie.id} />
+                ))}
+            </div>
+
+        </div>
+    )
 }
